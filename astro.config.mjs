836f11b2a9eeb1ff677e7b52f6db4d5f411db656
chenmoonmo <chenmoonmo@gmail.com>
2022-09-10 03:59:42 +0800

    fix: change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +6,6 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), partytown()]
+  base:'/paoluda',
+  integrations: [react(), tailwind(), partytown()],
 });
